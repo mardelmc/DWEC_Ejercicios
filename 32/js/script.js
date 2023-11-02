73ccf04b8da1fcde12fe0604ca9b1d801039bcba
mardelmc <mmorcob238@g.educaand.com>
2023-11-02 17:36:07 +0100

    32

--- 32/js/script.js
+++ 32/js/script.js
@@ -1,15 +1,42 @@
 window.addEventListener('DOMContentLoaded', main);
 function main() {
-    const enviar = document.querySelector("button");
-    enviar.addEventListener("click", valida());
-}
+    const enviar = document.querySelector("form");
+    enviar.addEventListener("submit", function(){
+        const telef = document.getElementById('telp');
+        if(!telef.textContent == ""){
+            const telefonoRegex = /^[6-9]\d{8}$/;
+            if (! telefonoRegex.test(telef)) {
+                console.log("El número de teléfono no es válido");
+                alert('Por favor, ingrese su tel');
+            }
+        } else {
+            alert("Esta vacio el tel")
+        }
+        
 
-function valida(){
-    //telefono
-    //dni
-    //email
-    console.log("boton pulsado");
-    const email = document.querySelector("input#mail");
-    email.validationError();
+        const dni = document.getElementById('dni');
+        if(!dni.textContent == ""){
+            const dniRegex = /^(\d{8})([A-Z])$/; 
+            if (dniRegex.test(dni)) {
+                const numeroDNI = dni.substring(0, 8); 
+                const letraProvida = dni.charAt(8);
+                const letrasPosibles = 'TRWAGMYFPDXBNJZSQVHLCKE';
+                const resto = numeroDNI % 23;
+                const letraCalculada = letrasPosibles.charAt(resto);
+                if (! letraProvida == letraCalculada){
+                    console.log("El número de dni no es válido");
+                    alert('Por favor, ingrese su dni');
+                }
+            }
+        }
 
+        const email = document.getElementById('mail');
+        if(!email.textContent == ""){
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(email)) {
+                console.log("El correo electrónico no es válido");
+                alert('Por favor, ingrese su email');
+            }
+        }
+    });
 }
