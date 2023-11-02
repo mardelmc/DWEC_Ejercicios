e416377c3ea81ff02403b1b5b128de06e338ee28
mariadmc <mmorcob238@g.educaand.es>
2023-11-02 13:45:05 +0100

    JS 1

--- 32/js/script.js
+++ 32/js/script.js
@@ -1,4 +1,15 @@
 window.addEventListener('DOMContentLoaded', main);
 function main() {
+    const enviar = document.querySelector("button");
+    enviar.addEventListener("click", valida());
+}
+
+function valida(){
+    //telefono
+    //dni
+    //email
+    console.log("boton pulsado");
+    const email = document.querySelector("input#mail");
+    email.validationError();
 
 }
