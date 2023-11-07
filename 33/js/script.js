72fa5d5beee8868669fa477b272e8530088bad06
mariadmc <mmorcob238@g.educaand.es>
2023-11-07 14:26:33 +0100

    33 +

--- 33/js/script.js
+++ 33/js/script.js
@@ -1,16 +1,62 @@
 window.addEventListener('DOMContentLoaded', main);
 function main() {
-    rellenaAnio(1965);
+    const anioSelect = document.getElementById('year');
+    rellenaSelectAnio(1965, anioSelect);
+
+
+    const enviar = document.querySelector("#btnEnv");
+    enviar.addEventListener("click", function(){
+
+    const nombre = document.getElementById("name");
+    comprobarNombre(nombre.value);
+
+    const contrasena = document.getElementById("pass");
+    const contrasenaV = document.getElementById("passV");
+    comprobarContrasena(contrasena.value, contrasenaV.value);
+    
+    const anyo = document.getElementById("year");
+    let seleccionado = anyo.options[document.querySelector("#year").selectedIndex];
+    comprobarAnyo(seleccionado.value);
+    
+
+
+    const formulario = document.getElementById('formulario');
+    if (/*y las demas*/true) formulario.submit();
+    });
+    const limpiar = document.querySelector('#btnLim');
+    limpiar.addEventListener("click",function(){
+        
+        /*.classList.remove("incorrecto");*/
+    })
 }
 
-function rellenaAnio(desde){
+function rellenaSelectAnio(desde, anioSelect){
     if (isNaN(desde)) desde = 1900;
-    const anioSelect = document.getElementById('year');
+    
     const actual = new Date().getFullYear();
     for (let anio = desde; anio <= actual; anio++) {
         const option = document.createElement('option');
         option.value = anio;
         option.textContent = anio;
+        option.id = anio;
         anioSelect.appendChild(option);
     }
+}
+function comprobarNombre(nombre){
+
+}
+function comprobarAlias(alias){
+    
+}
+function comprobarContrasena(contrasena, contrasenaV){
+    
+}
+function comprobarAnyo(anyo){
+
+}
+function comprobarSexo(sexo){
+
+}
+function comprobarAficiones(aficiones){
+
 }
