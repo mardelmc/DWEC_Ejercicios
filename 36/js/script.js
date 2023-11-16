2b16fcf188d75abd6fcc7d08ad5683e0c5bb1b5d
mardelmc <mmorcob238@g.educaand.com>
2023-11-16 12:33:15 +0100

    36 por fin funcional y arreglado joder hostia ya

--- 36/js/script.js
+++ 36/js/script.js
@@ -1,85 +1,85 @@
 window.addEventListener('DOMContentLoaded', main);
 function main() {
-    const anioSelect = document.getElementById('year');
+    const anioSelect = document.querySelector('#year');
     rellenaSelectanio(anioSelect);
     let guardados = [];
 
-    const enviar = document.querySelector("#btnEnv");
-    enviar.addEventListener("click", function(){
+    const enviar = document.querySelector('#btnEnv');
+    enviar.addEventListener('click', function(){
 
-        const nombre = document.getElementById("name");
-        if (comprobarNombre(nombre.value) == ""){
+        const nombre = document.querySelector('#name');
+        if (comprobarNombre(nombre.value) == ''){
             invalido(nombre);
         }
 
-        const alias = document.getElementById("nick");
-        if (comprobarAlias(alias.value) == ""){
+        const alias = document.querySelector('#nick');
+        if (comprobarNombre(alias.value) == ''){
             invalido(alias);
         }
-        alias.addEventListener("change", function(){
-            if (alias.value == guardados[1]) rellenar(guardados);
-        })
+        alias.addEventListener('change', function(){
+            let recuperados = consultarGalleta(alias.value); 
+            if (recuperados == "" ){
+                return false;
+            }
+        console.log(recuperados.split('#'));
+            rellenar(recuperados.split('#'));
+        });
 
-        const contrasenia = document.getElementById("pass");
-        const contraseniaV = document.getElementById("passV");
-        if (comprobarContrasenia(contrasenia.value, contraseniaV.value) == ""){
+        const contrasenia = document.querySelector('#pass');
+        const contraseniaV = document.querySelector('#passV');
+        if (comprobarContrasenia(contrasenia.value, contraseniaV.value) == ''){
             invalido(contrasenia);
             invalido(contraseniaV);
         };
         
-        const anio = document.getElementById("year");
-        let seleccionadoA = anio.options[document.querySelector("#year").selectedIndex];
-        if(comprobarAnio(seleccionadoA.value) == ""){
+        const anio = document.querySelector('#year');
+        let seleccionadoA = anio.options[document.querySelector('#year').selectedIndex];
+        if(comprobarAnio(seleccionadoA.value) == ''){
             invalido(anio);
         }
         
         let seleccionadoR = document.querySelector('input[name="sex"]:checked');
-        if(comprobarSexo(seleccionadoR) == ""){
-            const labelS = document.getElementById('labelS');
+        if(comprobarSexo(seleccionadoR) == ''){
+            const labelS = document.querySelector('#labelS');
             invalido(labelS);
         }
 
         let seleccionadoC = document.querySelectorAll('input[type="checkbox"]:checked');
-        if(comprobarAficiones(seleccionadoC) == ""){
-            const labelA = document.getElementById('labelA');
+        if(comprobarAficiones(seleccionadoC) == ''){
+            const labelA = document.querySelector('#labelA');
             invalido(labelA);
         }
 
 
-        const formulario = document.getElementById('formulario');
+        const formulario = document.querySelector('#formulario');
         let envio = true;
         for (let campo of formulario) {
-            if (campo.classList == "incorrecto") envio = false;
+            if (campo.classList == 'incorrecto') envio = false;
         }
-        if(labelS.classList == "incorrecto" && labelA.classList == "incorrecto") envio = false;
+        if(labelS.classList == 'incorrecto' && labelA.classList == 'incorrecto') envio = false;
         if (envio) {
-            alert("enviado");
-            //formulario.submit();
-            guardados = [nombre.value, alias.value, contrasenia.value, anio.value, seleccionadoR.value, seleccionadoC];
-            mandarGalleta("nombre",nombre.value);
-            mandarGalleta("alias",alias.value);
-            mandarGalleta("contrasenia",contrasenia.value);
-            mandarGalleta("anio",anio.value);
-            mandarGalleta("seleccionadoR",seleccionadoR.value);
-            let strSeleccionadoC = "";
-            for (let i = 0; i < seleccionadoC.length; i++){
-                strSeleccionadoC += seleccionadoC[i].value + ","
-            }
-            mandarGalleta("seleccionadoC",strSeleccionadoC);
+            alert('enviado');
+            formulario.submit();
+            let arraySeleccionadoC = Array.from(seleccionadoC);
+            let strSeleccionadoC = arraySeleccionadoC.map(item => item.value).join('&');
+            guardados = [alias.value, nombre.value, contrasenia.value, anio.value, seleccionadoR.value, strSeleccionadoC];
+            let strCookie = guardados.map(item => item).join('#');
+        console.log(guardados);
+            mandarGalleta(guardados[0], strCookie);
         }
 
     });
     const btnlimpiar = document.querySelector('#btnLim');
-    btnlimpiar.addEventListener("click", limpiar);
+    btnlimpiar.addEventListener('click', limpiar);
 }
 
 function limpiar() {
-    const formulario = document.querySelector("#formulario");
+    const formulario = document.querySelector('#formulario');
     for (let campo of formulario) {
-        campo.classList.remove("incorrecto");
+        campo.classList.remove('incorrecto');
     }
-    const labelS = document.getElementById('labelS').classList.remove("incorrecto");
-    const labelA = document.getElementById('labelA').classList.remove("incorrecto");
+    const labelS = document.querySelector('#labelS').classList.remove('incorrecto');
+    const labelA = document.querySelector('#labelA').classList.remove('incorrecto');
 }
 
 function rellenaSelectanio(anioSelect){
@@ -96,23 +96,27 @@ function rellenaSelectanio(anioSelect){
 }
 
 function comprobarNombre(nombre){
-    if (nombre == "") return "";
-}
-
-function comprobarAlias(alias){
-    if (alias == "") return "";
+    let salida = "";
+    if (nombre == '') salida = "";
+    const nombreRegex = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ]{2,10}$/;
+    if (nombreRegex.test(nombre)){ 
+        salida = nombre;
+    }
+    return salida;
 }
 
 function comprobarContrasenia(contrasenia, contraseniaV){
+    let salida = "";
     if (contrasenia == "" && contraseniaV == "" && contrasenia != contraseniaV) {
-        return "";
+        salida = "";
     } else {
-        return contrasenia;
+       salida = contrasenia;
     }
+    return salida;
 }
 
 function comprobarAnio(anio){
-    if (anio == "") return "";
+    if (anio == '') return "";
 }
 
 function comprobarSexo(sexo){
@@ -124,49 +128,54 @@ function comprobarAficiones(aficiones){
 }
 
 function invalido(nodo){
-    nodo.classList.add("incorrecto");
+    nodo.classList.add('incorrecto');
 }
 
-function rellenar(guardados) {
-    console.log("autocompleta");
-    let nombre = document.getElementById("name");
+function rellenar(datos) {
+console.log('autocompleta');
+    let nombre = document.querySelector('#name');
 
-    let contrasenia = document.getElementById("pass");
-    let contraseniaV = document.getElementById("passV");
-    let anio = document.getElementById("year");
+    let contrasenia = document.querySelector('#pass');
+    let contraseniaV = document.querySelector('#passV');
+    let anio = document.querySelector('#year');
     let seleccionadoR = document.querySelectorAll('input[name="sex"]');
     let seleccionadoC = document.querySelectorAll('input[type="checkbox"]');
 
-    nombre.value = guardados[0];
-    contrasenia.value = guardados[2];
-    contraseniaV.value = guardados[2];
+    nombre.value = datos[0];
+    contrasenia.value = datos[2];
+    contraseniaV.value = datos[2];
     for (let i = 0; i < anio.options.length; i++) {
-        if (anio.options[i].value == guardados[3]){
+        if (anio.options[i].value == datos[3]){
             let seleccion = anio.options[i];
             seleccion.selected = true;
         }
     }
 
     for (let i = 0; i < seleccionadoR.length; i++) {
-        if (seleccionadoR[i].value == guardados[4]){
+        if (seleccionadoR[i].value == datos[4]){
             let seleccion = seleccionadoR[i];
             seleccion.checked = true;
         }
     }
 
+    let aficiones = datos[5].split('&')
     for (let i = 0; i < seleccionadoC.length; i++) {
-        for (let j = 0; j < guardados[5].length; j++){
-            if (seleccionadoC[i].value == guardados[5][j].value){
+        for (let j = 0; j < aficiones.length; j++){
+            if (seleccionadoC[i].value == aficiones[j]){
                 let seleccion = seleccionadoC[i];
                 seleccion.checked = true;
             }
         }
     }
-
 }
-function mandarGalleta(nombre, valor, caducidad)
-{
- document.cookie = nombre + "=" + escape(valor)
- + ((caducidad == null) ? "" : ("; expires=" +
- caducidad.toGMTString()))
+function mandarGalleta(nombre, valores){
+    document.cookie = `${nombre}=${valores}`;
+console.log(valores);
 } 
+
+function consultarGalleta(nombre){
+    let galletitas = document.cookie.split('; ');
+    let cookieEncontrada = galletitas.find(galleta => galleta.startsWith(nombre + '='));
+    
+    return cookieEncontrada ? cookieEncontrada.split('=')[1] : '';
+}
